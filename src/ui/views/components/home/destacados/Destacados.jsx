f0f23150c7ae54982f8a335eddeb2d43ac23c89c
zzezequiel <ezequielzvirgzdins@gmail.com>
2022-11-22 21:05:56 +0100

    global api

--- src/ui/views/components/home/destacados/Destacados.jsx
+++ src/ui/views/components/home/destacados/Destacados.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import fetchData from '../../../../../api/Api'
 
 
-const url = "http://localhost:4000/porcelanatos";
+const url = "https://637d273f16c1b892ebc8c41e.mockapi.io/porcelanatos";
 
 const Destacados = () => {
 
@@ -24,15 +24,17 @@ const Destacados = () => {
     <div className='container'>
       <div className='row text-center'><h2>Destacados</h2></div>
         <div className='row mt-3'>
-            {destacados? destacados.map((product, z) => (
+            {destacados? destacados[0].porcelanatos.map((product, z) => (
                 
                   <div className="col padding_leri shadow-sm rounded cont-product cont-all" key={z}>
                    
-                    <Link to={`/detail-product/${product.id}`} className="product-box text-center link-off">
-                      <figure><img className="product-img" src={product.img} alt="#"/></figure>
-                      <div><h6 className='title-product text-center'>{product.title}</h6></div>
-                      </Link>
+                   <Link to={`/detail-product/${product.id}`} className="product-box text-center link-off">
+                     <figure><img className="product-img" src={product.img} alt="#"/></figure>
+                     <div><h6 className='title-product text-center'>{product.title}</h6></div>
+                     </Link>
                   </div>
+                
+                  
                 
             )) : "no destacados"}
         </div>
